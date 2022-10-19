5809792b7d802089a879cb295a01cb2f2240a81e
cakejune <tyq444@gmail.com>
2022-10-19 06:30:02 -0400

    commit tree bee8cc167f65849ed13a2a61a5e0a566db4bb14e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+addEventListener('DOMContentLoaded', (event) => 
+{
+    const dogContainer = document.querySelector('div#dog-image-container');
+    const dogBreedContainer = document.querySelector('ul#dog-breeds');
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data => data.message.forEach(createDog))
+
+    function createDog(dogImage)
+{
+    const dog = document.createElement('img');
+    dog.src = dogImage;
+    dogContainer.appendChild(dog);
+}
+
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(resp => resp.json())
+// .then(data => data.message.forEach(createBreed))
+.then(breeds => console.log(breeds.message))
+
+function createBreed(data)
+{
+    console.log("Creating Dog Breed.");
+    const dogBreed = document.createElement('li');
+    dogBreed.textContent = data;
+    dogBreedContainer.appendChild(dogBreed);
+}
+
+
+
+}
+);
